9452296e1c7ada32c16f92ee1e35a7a22b6c1b4f
AnthonyEbert <anthonyebert@gmail.com>
2019-02-18 15:09:59 +1000

    sG_10 and update to 0.1.0 , elegantised the code for Gaussian + Cauchy example.

--- R/sG_model.R
+++ R/sG_model.R
@@ -4,7 +4,7 @@ loss_sG <- function(param, inp){
 
   obs <- inp$obs
 
-  sim <- simulator_sGaussian(inp$Time, param = param, alpha = inp$alpha, mean_global = inp$mean_global, sigma_a = inp$sigma_a)
+  sim <- simulator_sGaussian(inp$Time, param = param, alpha_norm = inp$alpha_norm, alpha_cauchy = inp$alpha_cauchy, mean_global = inp$mean_global, sigma_a = inp$sigma_a)
 
   out <- distance_fun(obs, sim, registration = inp$registration, distance = inp$distance, method = inp$method, y_kmmd = inp$y_kmmd, var = inp$var, threshold = 6)
 
@@ -12,21 +12,24 @@ loss_sG <- function(param, inp){
 }
 
 #' @export
-simulator_sGaussian <- function(Time, param = c(1, 0.01, 0.7), alpha = c(10, 21, 25, 40, 50), basel = 0, mean_global = 0, sigma_a = 5){
+simulator_sGaussian <- function(Time, param = c(1, 0.7, 0.01), alpha_norm = c(20, 60), alpha_cauchy = c(40, 80), mean_global = 0, sigma_a = 5){
 
   sigma_global  <- param[1]
-  sigma_e       <- param[2]
-  scale_global  <- param[3]
+  scale_global  <- param[2]
+  sigma_e       <- param[3]
 
   mean_global   <- ifelse(is.na(param[4]), mean_global, param[4])
   sigma_a       <- ifelse(is.na(param[5]), sigma_a, param[5])
 
-  a       <- rnorm(length(alpha), mean_global, sigma_a)
+  a_norm        <- rnorm(length(alpha_norm), mean_global, sigma_a)
+  a_cauchy      <- rnorm(length(alpha_cauchy), mean_global, sigma_a)
 
   y <- rep(NA, length(Time))
 
   for(i in 1:length(Time)){
-    y[i] <- sum(density_fun(Time[i], mean = a + alpha, scale_param = c(sigma_global, scale_global), i = i)) + rnorm(1, 0, sigma_e) + basel * Time[i]
+    y[i] <- sum(dnorm(Time[i], mean = a_norm + alpha_norm, sd = sigma_global)) +
+      sum(dcauchy(Time[i], location = a_cauchy + alpha_cauchy, scale = scale_global)) +
+      rnorm(1, 0, sigma_e)
   }
 
   output <- as.matrix(data.frame(Time = Time, Output = y))
--- script/sG_script.R
+++ script/sG_script.R
@@ -11,24 +11,28 @@ run_number = 1
 
 set.seed(1)
 
-Time <- seq(0, 300, by = 0.5)
-alpha = seq(25, 300, by = 40)
-theta = c(1, 0.01, 0.7)
+Time <- seq(0, 200, by = 0.5)
+alpha = seq(20, 280, by = 20)
+theta = c(1, 0.7, 0.01)
+
+alpha_norm   <- alpha[seq(1,length(alpha), by = 2)]
+alpha_cauchy <- alpha[seq(2,length(alpha), by = 2)]
 
 n_runs = 4000
-pacc_final = 0.01
+pacc_final = 0.005
 
 var_mat <- diag(c(9, 0.01^2))
 
-y <- simulator_sGaussian(Time, param = theta, alpha = alpha)
+y <- simulator_sGaussian(Time, param = theta, alpha_norm = alpha_norm, alpha_cauchy = alpha_cauchy)
 y_kmmd <- EasyMMD::kmmd(y, var = var_mat)
-x <- simulator_sGaussian(Time, param = theta, alpha = alpha)
+x <- simulator_sGaussian(Time, param = theta, alpha_norm = alpha_norm, alpha_cauchy = alpha_cauchy)
 
 distance_fun(y, x, registration = TRUE, distance = "MMD", y_kmmd = y_kmmd, var = var_mat, threshold = 6)
 
 distance_args <- list(
   Time = Time,
-  alpha = alpha,
+  alpha_norm = alpha_norm,
+  alpha_cauchy = alpha_cauchy,
   obs = y,
   var = var_mat,
   threshold = 6,
@@ -36,7 +40,7 @@ distance_args <- list(
   distance = "MMD",
   method = "DP2",
   mean_global = 0,
-  sigma_a = 5
+  sigma_a = 2.5
 )
 
 distance_args$y_kmmd <- EasyMMD::kmmd(distance_args$obs, var = distance_args$var)
@@ -45,9 +49,9 @@ loss_sG(theta, distance_args)
 
 # ABC -----------------
 
-prior_sGaussian <- prior_unif(c(0, 0, 0), c(3, 0.02, 2), var_names = c("sigma_phi", "sigma_e", "scale_phi"), eval = FALSE)
+prior_sGaussian <- prior_unif(c(0, 0, 0), c(3, 2, 0.02), var_names = c("sigma[phi]", "rho[phi]", "sigma[epsilon]"), eval = FALSE)
 
-prior_sGaussian_eval <- prior_unif(c(0, 0, 0), c(3, 0.02, 2), var_names = c("sigma_phi", "sigma_e", "scale_phi"), eval = TRUE)
+prior_sGaussian_eval <- prior_unif(c(0, 0, 0), c(3, 2, 0.02), var_names = c("sigma[phi]", "rho[phi]", "sigma[epsilon]"), eval = TRUE)
 
 cov_func <- function(x){
   robust::covRob(x)$cov
@@ -64,7 +68,7 @@ cl <- "mclapply" #HPC
 
 ## sigma_a <- 5 -----------------
 
-distance_args$sigma_a <- 5
+distance_args$sigma_a <- 2.5
 
 ### Registration <- FALSE -------------
 
@@ -74,31 +78,31 @@ distance_args$registration <- FALSE
 
 distance_args$distance <- "FR"
 
-ABC_sG_5_RF_FR <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
+ABC_sG_RF_FR <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
   mutate(
     registration = distance_args$registration,
     distance     = distance_args$distance,
     sigma_a      = distance_args$sigma_a
   )
 
-save(ABC_sG_5_RF_FR, file = "ABC_sG_5_RF_FR.RData")
+save(ABC_sG_RF_FR, file = "ABC_sG_RF_FR.RData")
 
-summary(ABC_sG_5_RF_FR)
+summary(ABC_sG_RF_FR)
 
 #### Distance <- "MMD" -------------------
 
 distance_args$distance <- "MMD"
 
-ABC_sG_5_RF_MD <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
+ABC_sG_RF_MD <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
   mutate(
     registration = distance_args$registration,
     distance     = distance_args$distance,
     sigma_a      = distance_args$sigma_a
   )
 
-save(ABC_sG_5_RF_MD, file = "ABC_sG_5_RF_MD.RData")
+save(ABC_sG_RF_MD, file = "ABC_sG_RF_MD.RData")
 
-summary(ABC_sG_5_RF_MD)
+summary(ABC_sG_RF_MD)
 
 ### Registration <- TRUE -------------
 
@@ -108,39 +112,39 @@ distance_args$registration <- TRUE
 
 distance_args$distance <- "FR"
 
-ABC_sG_5_RT_FR <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
+ABC_sG_RT_FR <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
   mutate(
     registration = distance_args$registration,
     distance     = distance_args$distance,
     sigma_a      = distance_args$sigma_a
   )
 
-save(ABC_sG_5_RT_FR, file = "ABC_sG_5_RT_FR.RData")
+save(ABC_sG_RT_FR, file = "ABC_sG_RT_FR.RData")
 
-summary(ABC_sG_5_RT_FR)
+summary(ABC_sG_RT_FR)
 
 #### Distance <- "MMD" -------------------
 
 distance_args$distance <- "MMD"
 
-ABC_sG_5_RT_MD <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
+ABC_sG_RT_MD <- abc_start(prior_sGaussian, loss_sG, distance_args = distance_args, method = "RABC", control = abc_control, cl = cl) %>%
   mutate(
     registration = distance_args$registration,
     distance     = distance_args$distance,
     sigma_a      = distance_args$sigma_a
   )
 
-save(ABC_sG_5_RT_MD, file = "ABC_sG_5_RT_MD.RData")
+save(ABC_sG_RT_MD, file = "ABC_sG_RT_MD.RData")
 
-summary(ABC_sG_5_RT_MD)
+summary(ABC_sG_RT_MD)
 
 # Packaging everything up
 
 sessionInfo()
 
-ABC_sG_5 <- dplyr::bind_rows(ABC_sG_5_RF_FR, ABC_sG_5_RF_MD, ABC_sG_5_RT_FR, ABC_sG_5_RT_MD)
+ABC_sG <- dplyr::bind_rows(ABC_sG_RF_FR, ABC_sG_RF_MD, ABC_sG_RT_FR, ABC_sG_RT_MD)
 
-save(ABC_sG_5, file = "ABC_sG_5.RData")
+save(ABC_sG, file = "ABC_sG.RData")
 
 save.image()
 
